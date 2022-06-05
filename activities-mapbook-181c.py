f5833bdbdfd89695bd6f5d767723648bbe2d5b0d
Peter Flanders <pfflanders@gmail.com>
2022-06-05 04:51:06 -0700

    added support for esri geocode

--- activities-mapbook-181c.py
+++ activities-mapbook-181c.py
@@ -7,14 +7,8 @@ import arcpy
 import os
 from csv import reader
 import argparse
-try:
-    from geopy.geocoders import Nominatim
-    geopy_installed = True
-    geolocator = Nominatim(user_agent="activities-mapbook-181c")
-except ImportError:
-    print("missing geopy, so geocoding is unavailable")
-    geopy_installed = False
-
+import requests
+import json
 
 '''
 Folder Structure
@@ -28,6 +22,24 @@ Folder Structure
 |     (location of table to be created)
 '''
 
+
+# Set up Geocoding Services
+try:
+    from geopy.geocoders import Nominatim
+    geolocator = Nominatim(user_agent="activities-mapbook-181c")
+    geopy_installed = True
+except ImportError:
+    print("Geopy not installed. Continuing..")
+    geopy_installed = False
+
+try:    
+    with open('config.json') as file:
+        ARCGIS_DEVELOPER_API_KEY = json.load(file)["KEY"]
+    print(f'Arcgis Developer Key={ARCGIS_DEVELOPER_API_KEY}')
+except FileNotFoundError:
+    ARCGIS_DEVELOPER_API_KEY = None
+    if not geopy_installed:
+        print("The config file `config.json` was not created, and geopy was not installed. All Geocoding services are disabled.")
 
 
 # Set Folder Paths and Environment Variables
@@ -67,15 +79,15 @@ theLakeMapFrame.camera.setExtent(first_extent)
 theLakeMapFrame.camera.scale = theLakeMapFrame.camera.scale * 1.3
 
 parser = argparse.ArgumentParser(description='Create a mapbook from a location.')
-parser.add_argument('--coords', nargs=2, metavar=('lat', 'long'),
-                    help='two floats, a latitude and a longitude. Separated by a single space.')
+parser.add_argument('--lat',  type=float,
+                    help='One float, a latitude value.')
+parser.add_argument('--long', type=float,
+                    help='One float, a longitude value.')
 parser.add_argument('--address', type=str, 
                     help='An address description to be geocoded into a coordinate pair. MUST HAVE QUOTES (i.e. "125 East Lincoln Lane:)')
 parser.add_argument('-t', action='store_true',
                     help='An boolean to describe if the script it running as a test or full output. Type -t to run as test')
 args = parser.parse_args()
-
-print(args)
 
 if args.t:
     print("Running as Test. Using activies-data-TESTING.csv")
@@ -83,17 +95,57 @@ if args.t:
 
 # Orgin of our mapbook! 
 # Priority is (1) specified coords, (2) specified address, (3) default coords
-if args.coords:
-    start = (args.coords[1], args.coords[0])
-elif args.address and geopy_installed:
-    location = geolocator.geocode(args.address)
-    if location:
-        start = (location.longitude, location.latitude)
-        print(f'geocoded address is {start}')
-    else:
-        print('Location unable to be geocoded')
-        start = (-118.44497818287402, 34.06877178584797) # Center of UCLA!
-else:
+# The logic flow is untraditional below, but it is because I wish to try and fill the coordinates 
+# with multiple different sources before resorting to the default start location.
+coordinates_assigned = False
+
+if args.lat and args.long:
+    coordinates_assigned = True
+    start = (args.long, args.lat)
+
+if args.address and geopy_installed and not coordinates_assigned:
+    # use geopy I like it more
+    try:
+        location = geolocator.geocode(args.address)
+        if location:
+            start = (location.longitude, location.latitude)
+            print(f'Geopy geolocated the address at {start}')
+            coordinates_assigned = True
+    except Exception as e:
+        print(f"Geopy geocoding raised Exception {e}.")
+
+if args.address and ARCGIS_DEVELOPER_API_KEY and not coordinates_assigned:
+    # try to use esri's server
+    try:
+        # using some code from https://community.esri.com/t5/arcgis-api-for-python-blog/single-address-geocode-with-python/ba-p/902908 to geocode from arc servers
+        geoCodeUrl = f"https://geocode-api.arcgis.com/arcgis/rest/services/World/GeocodeServer/findAddressCandidates?token={ARCGIS_DEVELOPER_API_KEY}&f=json"
+        
+        #clean up the address for url encoding
+        address = args.address.replace(" ", "+")
+        address = address.replace(",", "%3B")
+        url = geoCodeUrl + "&address=" + address
+        print(f'url={url}')
+
+        #send address to geocode service
+        lookup = requests.get(url)
+        data = lookup.json()
+        print(f'data from esri server is: {data}')
+    except Exception as e:
+        print(f"ESRI geocoding service raised Exception {e}. ")
+
+    try:    
+        if data["candidates"]:
+            #woo hoo results
+            coords = data["candidates"][0]["location"]
+            start = coords["x"], coords["y"]
+            print(f'ESRI geolocated the address at {start}')
+            coordinates_assigned = True
+    except Exception as e:
+        print(f"ESRI geocoding service raised Exception {e}. It may be that the address provided is not accurate enough.")
+
+if not coordinates_assigned:
+    if args.address:
+        print('Default Coordinate being used. Potentially invalid address provided.')
     start = (-118.44497818287402, 34.06877178584797) # Center of UCLA!
 
 
